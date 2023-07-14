6eccc71c2ae2f9ce9db50dacf7e3b71de8baee80
Amandeep <asinghsanity@gmail.com>
2023-07-13 22:18:21 -0400

    Use functions in database.py

--- db/database.py
+++ db/database.py
@@ -6,66 +6,80 @@ from dotenv import load_dotenv
 load_dotenv()
 GLOBAL_DB = os.getenv('GLOBAL_DB')
 
-# Connect to / Create  DB
-conn = sqlite3.connect(GLOBAL_DB)
+def create_tables():
+    '''Functionality to create tables'''
+    # Connect to / Create  DB
+    conn = sqlite3.connect(GLOBAL_DB)
 
-# Cursor
-c = conn.cursor()
+    # Cursor
+    c = conn.cursor()
 
-# Create tables
+    # Create tables
+    users_sql = '''
+        CREATE TABLE IF NOT EXISTS users(
+            username TEXT NOT NULL UNIQUE,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            PRIMARY KEY (username)
+        );
+    '''
 
-USERS_SQL = '''
-    CREATE TABLE IF NOT EXISTS users(
-        username TEXT NOT NULL UNIQUE,
-        first_name TEXT NOT NULL,
-        last_name TEXT NOT NULL,
-        PRIMARY KEY (username)
-    );
-'''
+    bank_account_sql = '''
+        CREATE TABLE IF NOT EXISTS bank_account(
+            account_number TEXT NOT NULL UNIQUE,
+            username TEXT,
+            account_type TEXT NOT NULL,
+            bank_name TEXT NOT NULL,
+            routing_number TEXT NOT NULL,
+            account_balance REAL DEFAULT 0,
+            PRIMARY KEY (account_number),
+            FOREIGN KEY (username) REFERENCES users(username)
+        );
+    '''
 
-BANK_ACCOUNT_SQL = '''
-    CREATE TABLE IF NOT EXISTS bank_account(
-        account_number INTEGER NOT NULL UNIQUE,
-        username TEXT,
-        account_type TEXT NOT NULL,
-        bank_name TEXT NOT NULL,
-        routing_number INTEGER NOT NULL,
-        account_balance REAL DEFAULT 0,
-        PRIMARY KEY (account_number),
-        FOREIGN KEY (username) REFERENCES users(username)
-    );
-'''
+    checking_sql = '''
+        CREATE TABLE IF NOT EXISTS checking (
+        account_number TEXT NOT NULL,
+        transaction_id INTEGER,
+        date TEXT NOT NULL,
+        description TEXT,
+        payment_amt REAL NOT NULL DEFAULT 0,
+        deposit_amt REAL NOT NULL DEFAULT 0,
+        balance REAL NOT NULL DEFAULT 0,
+        PRIMARY KEY (account_number, transaction_id),
+        FOREIGN KEY (account_number) REFERENCES bank_account(account_number)
+        );
+    '''
 
-CHECKING_SQL = '''
-    CREATE TABLE IF NOT EXISTS checking (
-    account_number INTEGER NOT NULL,
-    transaction_id INTEGER,
-    date TEXT NOT NULL,
-    description TEXT,
-    payment_amt REAL NOT NULL DEFAULT 0,
-    deposit_amt REAL NOT NULL DEFAULT 0,
-    balance REAL NOT NULL DEFAULT 0,
-    PRIMARY KEY (account_number, transaction_id),
-    FOREIGN KEY (account_number) REFERENCES bank_account(account_number)
-    );
-'''
+    # Execute Queries
+    c.execute(users_sql)
+    c.execute(bank_account_sql)
+    c.execute(checking_sql)
 
-# Drop tables if needed
-# DROP_USERS_SQL = '''DROP TABLE IF EXISTS users'''
-# DROP_BANK_ACCOUNT_SQL = '''DROP TABLE IF EXISTS bank_account'''
-# DROP_CHECKING_SQL = '''DROP TABLE IF EXISTS checking'''
-# c.execute(DROP_USERS_SQL)
-# c.execute(DROP_BANK_ACCOUNT_SQL)
-# c.execute(DROP_CHECKING_SQL)
+    # Commit sql
+    conn.commit()
 
+    # Close connection
+    conn.close()
 
-# Execute Queries
-c.execute(USERS_SQL)
-c.execute(BANK_ACCOUNT_SQL)
-c.execute(CHECKING_SQL)
+def delete_tables():
+    '''Functionality to drop all tables if needed'''
 
-# Commit sql
-conn.commit()
+    # Connect to / Create  DB
+    conn = sqlite3.connect(GLOBAL_DB)
 
-# Close connection
-conn.close()
+    # Cursor
+    c = conn.cursor()
+    drop_users_sql = '''DROP TABLE IF EXISTS users'''
+    drop_bank_account_sql = '''DROP TABLE IF EXISTS bank_account'''
+    drop_checking_sql = '''DROP TABLE IF EXISTS checking'''
+
+    c.execute(drop_users_sql)
+    c.execute(drop_bank_account_sql)
+    c.execute(drop_checking_sql)
+
+    # Commit sql
+    conn.commit()
+
+    # Close connection
+    conn.close()
