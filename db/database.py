cdac4459c5b9abd95a38c5afe23d25a77de6b182
Amandeep <asinghsanity@gmail.com>
2023-07-13 20:05:21 -0400

    Design decision to change accounts to bank_account Also added in account balance to the table.

--- db/database.py
+++ db/database.py
@@ -18,13 +18,14 @@ USERS_SQL = '''
     );
 '''
 
-ACCOUNTS_SQL = '''
-    CREATE TABLE IF NOT EXISTS accounts(
+BANK_ACCOUNT_SQL = '''
+    CREATE TABLE IF NOT EXISTS bank_account(
         account_number INTEGER NOT NULL UNIQUE,
         username TEXT,
         account_type TEXT NOT NULL,
         bank_name TEXT NOT NULL,
         routing_number INTEGER NOT NULL,
+        account_balance REAL DEFAULT 0,
         PRIMARY KEY (account_number),
         FOREIGN KEY (username) REFERENCES users(username)
     );
@@ -40,22 +41,22 @@ CHECKING_SQL = '''
     deposit_amt REAL NOT NULL DEFAULT 0,
     balance REAL NOT NULL DEFAULT 0,
     PRIMARY KEY (account_number, transaction_id),
-    FOREIGN KEY (account_number) REFERENCES accounts(account_number)
+    FOREIGN KEY (account_number) REFERENCES bank_account(account_number)
     );
 '''
 
 # Drop tables if needed
 # DROP_USERS_SQL = '''DROP TABLE IF EXISTS users'''
-# DROP_ACCOUNTS_SQL = '''DROP TABLE IF EXISTS accounts'''
+# DROP_BANK_ACCOUNT_SQL = '''DROP TABLE IF EXISTS bank_account'''
 # DROP_CHECKING_SQL = '''DROP TABLE IF EXISTS checking'''
 # c.execute(DROP_USERS_SQL)
-# c.execute(DROP_ACCOUNTS_SQL)
+# c.execute(DROP_BANK_ACCOUNT_SQL)
 # c.execute(DROP_CHECKING_SQL)
 
 
 # Execute Queries
 c.execute(USERS_SQL)
-c.execute(ACCOUNTS_SQL)
+c.execute(BANK_ACCOUNT_SQL)
 c.execute(CHECKING_SQL)
 
 # Commit sql
